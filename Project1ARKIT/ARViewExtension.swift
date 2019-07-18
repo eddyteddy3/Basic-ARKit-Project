e12bec55bc4ffe6de4a3a25518dd263a19294732
Mouzzam <moazzamtahir81@gmail.com>
2019-07-18 16:36:08 +0500

    The comments has been added to give the conceptual view of what is happening.

--- Project1ARKIT/ARViewExtension.swift
+++ Project1ARKIT/ARViewExtension.swift
@@ -6,61 +6,81 @@
 //  Copyright © 2019 Moazzam Tahir. All rights reserved.
 //
 
-import ARKit
 import SceneKit
+import ARKit
 
 extension ViewController: ARSCNViewDelegate {
     
     func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
         
-        guard anchor is ARPlaneAnchor else { return }
-            print("Plane Detected")
-            
-            let planeAnchor = anchor as! ARPlaneAnchor //downcasted as planeAnchor
-            
-            let planeNode = addPlaneNode(planeAnchor: planeAnchor)
-            node.addChildNode(planeNode )
+        //if the anchor is plane then proceed
+        guard anchor is ARPlaneAnchor else {return}
+        print("Plane Detected")
+        
+        let plane = anchor as! ARPlaneAnchor
+        
+        let planeNode = createPlaneNode(plane)
+        
+        //adding the child node to the root node "node"
+        node.addChildNode(planeNode)
+        
     }
     
     func renderer(_ renderer: SCNSceneRenderer, didUpdate node: SCNNode, for anchor: ARAnchor) {
         
-        guard anchor is ARPlaneAnchor else { return }
+        guard anchor is ARPlaneAnchor else {return}
         
-        print("plane detected")
+        print("plane updated")
         
-        let plane = anchor as! ARPlaneAnchor
+        let planeAnchor = anchor as! ARPlaneAnchor
         
+        //we are removing the child nodes becuase everytime the scene update the new updated size of node will be added, so previous child will be removed
         node.enumerateChildNodes { (childNode, _) in
-            childNode.removeFromParentNode()
+            childNode.removeFromParentNode() //this child node will be removed from parent's node
         }
         
-        let planeNode = addPlaneNode(planeAnchor: plane)
+        //adding the updated child node to the parent node
+        let planeNode = createPlaneNode(planeAnchor)
         node.addChildNode(planeNode)
+        
     }
     
+    //the scene was detecting more than one anchors for a plane. To remove the other anchor the following method was used.
     func renderer(_ renderer: SCNSceneRenderer, didRemove node: SCNNode, for anchor: ARAnchor) {
         
         guard anchor is ARPlaneAnchor else {return}
         
-        print("Plane removed")
-        
         node.enumerateChildNodes { (childNode, _) in
             childNode.removeFromParentNode()
         }
         
     }
     
-    func addPlaneNode(planeAnchor: ARPlaneAnchor) -> SCNNode {
+    //this function will take plane as parameter and return the node attached in result which will attach to the node
+    func createPlaneNode(_ planeAnchor: ARPlaneAnchor) -> SCNNode {
         
+        //setting the geometry for the plane using anchor
         let plane = SCNPlane(width: CGFloat(planeAnchor.extent.x), height: CGFloat(planeAnchor.extent.z))
         
+        //giving texture to the plane to show it visually on the screen
         plane.firstMaterial?.diffuse.contents = UIImage(named: "grid")
+        
+        //making the grid image double sided to show completely
         plane.firstMaterial?.isDoubleSided = true
         
+        //now creating node with geometry of plane we just defined
         let planeNode = SCNNode(geometry: plane)
+        
+        //now placing the node in the center of the detect surface
         planeNode.position = SCNVector3(planeAnchor.center.x, planeAnchor.center.y, planeAnchor.center.z)
+        
+        //rotating the plane node to 90 degrees as it will display standing in the middle
+        //euler angles are used to position, orientation the node
         planeNode.eulerAngles.x = GLKMathDegreesToRadians(-90)
         
+        //finally returning the node that will be place on detected surface
         return planeNode
+        
     }
+    
 }
